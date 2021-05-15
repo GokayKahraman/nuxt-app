7416f8c28b44a2fb68ff838d9d3dae5108689ec1
gokay <gokay.kahraman@yandex.com>
2021-05-15 19:12:17 +0300

    butonlar

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,6 +1,48 @@
 <template>
   <div class="container">
-    
+    <h5>Buton Classları</h5>
+		<p><button class="btn btn-primary">Udemy Bootstrap 4 Kursu</button> </p>
+		<p><button class="btn btn-secondary">Udemy Bootstrap 4 Kursu</button> </p>
+		<p><button class="btn btn-success">Udemy Bootstrap 4 Kursu</button> </p>
+		<p><button class="btn btn-danger">Udemy Bootstrap 4 Kursu</button> </p>
+		<p><button class="btn btn-warning">Udemy Bootstrap 4 Kursu</button> </p>
+		<p><button class="btn btn-info">Udemy Bootstrap 4 Kursu</button> </p>
+		<p><button class="btn btn-light">Udemy Bootstrap 4 Kursu</button> </p>
+		<p><button class="btn btn-dark">Udemy Bootstrap 4 Kursu</button> </p>
+
+		<h5>Buton Tipleri</h5>
+		<p><button type="buton" class="btn btn-primary">Udemy Bootstrap 4 Kursu</button> </p>
+		<p><button type="submit" class="btn btn-primary">Udemy Bootstrap 4 Kursu</button> </p>
+		<p><button type="reset" class="btn btn-primary">Udemy Bootstrap 4 Kursu</button> </p>
+
+		<h5>Buton Dış Çizgi Kenarlık</h5>
+
+		<p><button class="btn btn-outline-primary">Udemy Bootstrap 4 Kursu</button> </p>
+		<p><button class="btn btn-outline-secondary">Udemy Bootstrap 4 Kursu</button> </p>
+		<p><button class="btn btn-outline-success">Udemy Bootstrap 4 Kursu</button> </p>
+		<p><button class="btn btn-outline-danger">Udemy Bootstrap 4 Kursu</button> </p>
+		<p><button class="btn btn-outline-warning">Udemy Bootstrap 4 Kursu</button> </p>
+		<p><button class="btn btn-outline-info">Udemy Bootstrap 4 Kursu</button> </p>
+		<p><button class="btn btn-outline-light">Udemy Bootstrap 4 Kursu</button> </p>
+		<p><button class="btn btn-outline-dark">Udemy Bootstrap 4 Kursu</button> </p>
+
+
+		<h5>Buton Boyutları</h5>
+
+		<p><button class="btn btn-primary btn-lg">Udemy Bootstrap 4 Kursu</button> </p>
+		<p><button class="btn btn-primary btn-sm">Udemy Bootstrap 4 Kursu</button> </p>
+		<p><button class="btn btn-primary btn-sm btn-block">Udemy Bootstrap 4 Kursu</button> </p>
+
+		<div class="col-md-4">
+			<p><button class="btn btn-primary btn-sm btn-block">Udemy Bootstrap 4 Kursu</button> </p>
+
+		</div>
+
+
+		<h5>Buton Aktif ve Pasif Özellikleri</h5>
+
+		<p><button class="btn btn-primary btn-lg active">Udemy Bootstrap 4 Kursu</button> </p>
+		<p><button class="btn btn-primary btn-sm disabled">Udemy Bootstrap 4 Kursu</button> </p>
   </div>
 </template>
 
