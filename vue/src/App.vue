250811b5da8165bcf96e91c9df996d871f482a46
gokay <gokay.kahraman@yandex.com>
2021-05-15 21:14:22 +0300

    group

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,11 +1,64 @@
 <template>
   <div class="container">
-    <div class="btn-group">
+    <div class="btn-group" role="group" aria-label="butonlarımız">
       <button class="btn btn-secondary">Udemy Bootstrap 4 Kursu</button>
       <button class="btn btn-secondary">Udemy wegrwge 4 Kursu</button>
       <button class="btn btn-secondary">Udemy Bootssgdgsdtrap 4 Kursu</button>
-      <button class="btn btn-secondary">Udemy gsd 4 Kursu</button>
     </div>
+
+    <br>
+    <br>
+
+    <div class="btn-toolbar" role="toolbar" aria-label="butonlarımız">
+
+      <div class="btn-group mr-2" role="group" aria-label="butonlarımız">
+        <button class="btn btn-primary">1</button>
+        <button class="btn btn-primary">2</button>
+        <button class="btn btn-primary">3</button>
+        <button class="btn btn-primary">4</button>
+      </div>
+
+      <div class="btn-group mr-2" role="group" aria-label="butonlarımız">
+        <button class="btn btn-primary">5</button>
+        <button class="btn btn-primary">6</button>
+        <button class="btn btn-primary">7</button>
+      </div>
+
+    </div>
+    <br>
+
+    <div class="dropdown">
+        <button class="btn btn-success dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Dropdown button
+        </button>
+        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+            <a class="dropdown-item" href="#">Action</a>
+            <a class="dropdown-item" href="#">Another action</a>
+            <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+    </div>
+
+    <br>
+    <br>
+
+    <h1>asgfasdfasdf <span class="badge badge-primary"> fasfdagsdfgssfadsfa</span></h1>
+    <!-- bootstrap te bir sıkıntı var linklerde -->
+
+    <div class="alert alert-primary">kaydınız başarılı <a href="#" class="alert-link">giriş yapınız</a></div>
+    <br>
+
+    <div class="jumbotron">
+      <h1 class="display-3">Başlık fasfa</h1>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque aspernatur fuga except</p>
+      <br>
+      <p class="lead">deneme ksflaksfşlajfalşfkalsf</p>
+      <!-- lead de bozuk -->
+      <br>
+      <a href="#" class="btn btn-success btn-lg">kayıt ol</a>
+      
+    </div>
+
+    <br>
   </div>
 </template>
 
