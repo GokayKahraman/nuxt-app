297f55b921ca96f6aeac8d7689dbd76b17eb8fad
gokay <gokay.kahraman@yandex.com>
2021-05-16 20:29:27 +0300

    formlar

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,5 +1,61 @@
 <template>
   <div class="container">
+    <div class="form-group">
+				<input type="text" class="form-control" name="">
+			</div>
+			<div class="form-group">
+				<input type="email" class="form-control" name="">
+			</div>
+			<div class="form-group">
+				<select class="form-control">
+					<option>Kurs 1</option>
+					<option>Kurs 1</option>
+					<option>Kurs 1</option>
+					<option>Kurs 1</option>
+				</select>
+			</div>
+
+			<div class="form-group">
+				<select multiple="" class="form-control">
+					<option>Kurs 1</option>
+					<option>Kurs 1</option>
+					<option>Kurs 1</option>
+					<option>Kurs 1</option>
+					<option>Kurs 1</option>
+					<option>Kurs 1</option>
+					<option>Kurs 1</option>
+				</select>
+			</div>
+
+			<div class="form-group">
+				<textarea class="form-control" rows="10"></textarea>
+
+			</div>
+
+			<div class="form-group">
+				<input type="file" name="">
+				
+			</div>
+
+			<div class="form-group">
+				<input type="text" readonly class="form-control" value="Bu giriş kapalıdır" name="">
+				<input type="text" readonly class="form-control-plaintext" value="Bu giriş kapalıdır" name="">
+				<input type="text" disabled class="form-control" value="Bu giriş kapalıdır" name="">
+
+			</div>
+
+			<div class="form-check form-check-inline">
+				<label class="form-check-label">
+					<input type="checkbox" class="form-check-input" name=""> Elma</label>
+			</div>
+			<div class="form-check form-check-inline">
+				<label class="form-check-label">
+				<input type="checkbox" class="form-check-input" name=""> Armut</label>
+			</div>
+			<div class="form-check form-check-inline">
+				<label class="form-check-label">
+				<input type="checkbox" class="form-check-input" name=""> Üzüm</label>
+			</div>
     <form>
       
       <div class="form-row">
