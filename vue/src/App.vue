fbd01df2cd2bc3b78a6cc379cf9ab3cee3689fe5
gokay <gokay.kahraman@yandex.com>
2021-05-16 20:20:09 +0300

    inputlar

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,78 +1,19 @@
 <template>
   <div class="container">
-    
-    <div class="card" style="width: 350px;">
-    <img class="card-img-top" src="./img/card1.jpg">
-      <div class="card-body">
-        <h4 class="card-title">ana başlık</h4>
-        <p class="card-text">gsdökglşiksd,gişlskdfşgisdflkasidşlfsişadflşsidfsfds</p>
-        <a href="#" class="btn btn-success">devamke</a>
-      </div>
-    </div>
-<br>
-<br>
-    <div class="col-md-4">
-      <div class="card">
-        <div class="card-body">
-          <h4 class="card-title">kartvizitim</h4>
-          <h6 class="card-subtitle text-muted"></h6>
-          <p>bootstrap öğreniyorum yeeeey</p>
-          <a href="#" class="card-link">bilgilerim </a>
-          <a href="#" class="card-link">bana ulaşma </a>
-          <ul class="list-group list-group-flush my-4">
-            <li class="list-group-item">ileri seviye dolandırıcılık</li>
-            <li class="list-group-item">ileri seviye dolandırıcılık</li>
-            <li class="list-group-item">ileri seviye dolandırıcılık</li>
-          </ul>
-        </div>
-      </div>
+    <div class="input-group">
+      <span class="input-group-addon">baş</span>
+      <input type="text" class="form-control">
+      <span class="input-group-addon">son</span>
     </div>
     <br>
     <br>
-    <div class="card">
-      <div class="card-header">kart özellikleri</div>
-      <div class="card-body">
-        <h4 class="card-title">wep programlama</h4>
-        <p>Lorem ipsum dolor sit amet, consectetur elit modi aspernatur necessitatibus ad mole</p>
-      </div>
-    </div>
-    <br>
-    <br>
-    <div class="card">
-      <div class="card-header">kart özellikleri</div>
-      <div class="card-body">
-        <blockquote class="blockquote">
-          <p>giakşlkfgşlsadkgşlaskdgşlkaslişkgslşkigasdklşaisgklşaigkşligslşkigs</p>
-          <footer class="blockquote">gökay kahraman tarafından söylendi tarzı bi yazı</footer>
-        </blockquote>
-      </div>
-    </div>
-    <br>
-    <br>
-    <div class="card bg-info text-white border-danger">
-      <div class="card-header">başlık</div>
-      <div class="card-body">
-        <h4>kart stilleri</h4>
-        <p class="card-text">gşnçlkasjdnglşkajsdfglkhjnaedloghnjwoılregjn agıjdgoıw ejrgoıaw</p>
-      </div>
-    </div>
-<br>
-<br>
-    <div class="card-group" style="width: 350px;">
-      <div class="card">
-        <img src="./img/card1.jpg" class="card-img-top">
-        <div class="card-body">
-          <h4 class="card-title">kurs başlığı</h4>
-          <p class="card-text">lorem flana fasfşlasfna</p>
-        </div>
-      </div>
-      <div class="card">
-        <img src="./img/card1.jpg" class="card-img-top">
-        <div class="card-body">
-          <h4 class="card-title">kurs başlığı</h4>
-          <p class="card-text">lorem flana fasfşlasfna</p>
-        </div>
-      </div>
+    <div class="input-group">
+      <span class="input-group-addon">baş</span>
+      <span class="input-group-addon"><input type="checkbox"></span>
+      <span class="input-group-addon"><input type="radio"></span>
+      <span class="input-group-btn"><button class="btn btn-secondary">buton</button></span>
+      <input type="text" class="form-control">
+      <span class="input-group-addon">son</span>
     </div>
   </div>
 </template>
