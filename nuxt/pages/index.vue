e1a5947f1996248f87c57e32aa94082101512e08
gokay <gokay.kahraman@yandex.com>
2021-05-30 21:08:59 +0300

    first commit

---
+++ nuxt/pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div >
+    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet nobis blanditiis asperiores quas, officia omnis itaque necessitatibus voluptatem. Minus vel laboriosam quod consequuntur architecto doloribus magnam nemo, ex aut ut.
+  </div>
+</template>
+
+<script>
+const axios = require("axios")
+export default {
+    mounted: function() {
+    axios.get("https://animechan.vercel.app/api/quotes")
+      
+    },
+  
+} 
+</script>
+
+<style>
+
+</style>
